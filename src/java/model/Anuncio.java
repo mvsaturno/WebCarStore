1eb412723456f0e85c1a17234ec4dd249046cf5c
Fabio-CS <fabio.cs3mx@gmail.com>
2013-01-06 22:00:19 -0200

    DAO Anuncio pronta, falta os tratas

--- src/java/model/Anuncio.java
+++ src/java/model/Anuncio.java
@@ -12,13 +12,47 @@ public class Anuncio {
     private int id;
     private Veiculo veiculo;
     private String data_inicio;
-
-    public Anuncio() {
+    private int status;
+    private double valor_anuncio;
+    private int destaque;
+    private Revenda revenda;
+    
+    public Anuncio(){
     }
-
-    public Anuncio(Veiculo veiculo, String data_inicio) {
+    
+    public Anuncio(Veiculo veiculo, String data_inicio, int status, double valor_anuncio, int destaque, Revenda revenda) {
         this.veiculo = veiculo;
         this.data_inicio = data_inicio;
+        this.status = status;
+        this.valor_anuncio = valor_anuncio;
+        this.destaque = destaque;
+        this.revenda = revenda;
+    }
+
+    public Anuncio(int id, Veiculo veiculo, String data_inicio, int status, double valor_anuncio, int destaque, Revenda revenda) {
+        this.id = id;
+        this.veiculo = veiculo;
+        this.data_inicio = data_inicio;
+        this.status = status;
+        this.valor_anuncio = valor_anuncio;
+        this.destaque = destaque;
+        this.revenda = revenda;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+
+    public void setVeiculo(Veiculo veiculo) {
+        this.veiculo = veiculo;
     }
 
     public String getData_inicio() {
@@ -29,20 +63,36 @@ public class Anuncio {
         this.data_inicio = data_inicio;
     }
 
-    public Veiculo getVeiculo() {
-        return veiculo;
+    public int getStatus() {
+        return status;
     }
 
-    public int getId() {
-        return id;
+    public void setStatus(int status) {
+        this.status = status;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public double getValor_anuncio() {
+        return valor_anuncio;
     }
 
-    public void setVeiculo(Veiculo veiculo) {
-        this.veiculo = veiculo;
+    public void setValor_anuncio(double valor_anuncio) {
+        this.valor_anuncio = valor_anuncio;
+    }
+
+    public int getDestaque() {
+        return destaque;
+    }
+
+    public void setDestaque(int destaque) {
+        this.destaque = destaque;
+    }
+
+    public Revenda getRevenda() {
+        return revenda;
+    }
+
+    public void setRevenda(Revenda revenda) {
+        this.revenda = revenda;
     }
 
     @Override
