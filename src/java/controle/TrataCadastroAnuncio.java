09236bed3f01f43de35a872b08a17d1a129e5dd7
mv_sa_000 <mv_sa_000@saturno>
2013-01-17 06:12:21 -0200

    sql Properties da pergunta e resposta

--- src/java/controle/TrataCadastroAnuncio.java
+++ src/java/controle/TrataCadastroAnuncio.java
@@ -10,11 +10,17 @@ import dao.VeiculoDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpSession;
 import model.Anuncio;
 import model.Revenda;
+import model.Usuario;
 import model.Veiculo;
 
 /**
@@ -31,19 +37,23 @@ public class TrataCadastroAnuncio extends Comando {
             getResponse().setContentType("text/html");
 
             HttpSession session = getRequest().getSession(false);
-
+            Usuario usuarioLogado = (Usuario) session.getAttribute("usuario");
+            
             int id_veiculo = Integer.parseInt(getRequest().getParameter("veiculo_select_cad"));
             String data_inicio = getRequest().getParameter("data_inicio_anuncio_cad");
+            Date data = new SimpleDateFormat("dd/MM/yyyy").parse(data_inicio);  
+            String dataBanco = new SimpleDateFormat("dd-MM-yyyy").format(data);
+            
             int status = Integer.parseInt(getRequest().getParameter("status_select_cad"));
             double valor_anuncio = Double.parseDouble(getRequest().getParameter("valor_anuncio_cad"));
             int destaque = Integer.parseInt(getRequest().getParameter("status_anuncio_cad"));
-            int id_revenda = Integer.parseInt(getRequest().getParameter("id_anuncio_revenda"));
+            int id_revenda = usuarioLogado.getRevenda().getId();
 
             Veiculo veiculo = (Veiculo) new VeiculoDAO().pesquisarChave(id_veiculo);
             Revenda revenda = (Revenda) new RevendaDAO().pesquisarChave(id_revenda);
             
             
-            Anuncio anun = new Anuncio(veiculo,data_inicio,status,valor_anuncio,destaque,revenda);
+            Anuncio anun = new Anuncio(veiculo,dataBanco,status,valor_anuncio,destaque,revenda);
           
             AnuncioDAO cadAnuncio = new AnuncioDAO();
 
@@ -59,6 +69,8 @@ public class TrataCadastroAnuncio extends Comando {
             RequestDispatcher rd = getRequest().getRequestDispatcher("/sistema.jsp");
             rd.forward(getRequest(), getResponse());
 
+        } catch (ParseException ex) {
+            Logger.getLogger(TrataCadastroAnuncio.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
             throw new ServletException(ex);
         }
